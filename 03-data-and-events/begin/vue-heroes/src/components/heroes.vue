5e71e835c39268045ddac69ecfdd59e53db1db34
Niki <nikitheofilaktidou@gmail.com>
2022-06-23 18:00:30 +0300

    some more powder

--- 03-data-and-events/begin/vue-heroes/src/components/heroes.vue
+++ 03-data-and-events/begin/vue-heroes/src/components/heroes.vue
@@ -7,41 +7,56 @@
       <div class="column is-8">
         <div class="card edit-detail">
           <header class="card-header">
-            <p class="card-header-title"></p>
+            <p class="card-header-title">{{ hero.firstName }}</p>
           </header>
           <div class="card-content">
             <div class="content">
               <div class="field">
                 <label class="label" for="id">id</label>
-                <label class="input" id="id" readonly></label>
+                <label class="input" id="id" readonly>{{ hero.id }}</label>
               </div>
               <div class="field">
                 <label class="label" for="firstName">first name</label>
-                <input class="input" id="firstName" />
+                <input class="input" id="firstName" v-model="hero.firstName"/>
               </div>
               <div class="field">
                 <label class="label" for="lastName">last name</label>
-                <input class="input" id="lastName" />
+                <input class="input" id="lastName" v-model="hero.lastName"/>
               </div>
               <div class="field">
                 <label class="label" for="description">description</label>
-                <textarea class="input" id="description" type="text" />
+                <textarea class="input" id="description" type="text" v-model="hero.description"/>
               </div>
               <div class="field">
                 <label class="label">cape color</label>
                 <label class="radio" for="color-red">
-                  <input type="radio" id="color-red" value="red" />
-                  red
+                  <input 
+                  type="radio" 
+                  id="color-red" 
+                  value="#ff2e82" 
+                  v-model="hero.capeColor"
+                  />
+                  pink
                 </label>
                 <label class="radio" for="color-blue">
-                  <input type="radio" id="color-blue" value="blue" />
-                  blue
+                  <input 
+                  type="radio" 
+                  id="color-blue" 
+                  value="#1f6cc4" 
+                  v-model="hero.capeColor"
+                  />
+                  nice blue c:
                 </label>
                 <label class="radio" for="color-green">
-                  <input type="radio" id="color-green" value="green" />
-                  green
+                  <input 
+                  type="radio" 
+                  id="color-green" 
+                  value="#60e640" 
+                  v-model="hero.capeColor"
+                  />
+                  bug green
                 </label>
-                <div class="color-line"></div>
+                <div class="color-line" :style="{ 'background-color': hero.capeColor }"></div>
               </div>
               <div class="field">
                 <label for="power">
@@ -60,23 +75,26 @@
               <div class="field">
                 <label class="checkbox" for="active">
                   active
-                  <input type="checkbox" class="is-primary" id="active" />
+                  <input type="checkbox" class="is-primary" id="active" 
+                  v-model="hero.active"/>
                 </label>
               </div>
             </div>
           </div>
           <footer class="card-footer">
-            <button class="link card-footer-item cancel-button">
+            <button class="link card-footer-item cancel-button" @click="cancelHero">
               <i class="fas fa-undo"></i>
               <span>Cancel</span>
             </button>
-            <button class="link card-footer-item">
+            <button class="link card-footer-item save-button" @click="saveHero">
               <i class="fas fa-save"></i>
               <span>Save</span>
             </button>
           </footer>
         </div>
-        <div class="notification is-info"></div>
+        <div class="notification is-info">
+          <pre>{{ message }}</pre>
+          </div>
       </div>
     </div>
   </div>
@@ -85,5 +103,27 @@
 <script>
 export default {
   name: 'Heroes',
+ data() {
+  return {
+    hero: {
+     id: 34, 
+     firstName:'Niki',
+     lastName:'Theof',
+     description:'Water Walker',
+     capeColor:'Blue',
+     power:'',
+     active:true,
+    },
+    message:'',
+  }
+ },
+  methods: {
+    cancelHero() {
+      this.message = '';      
+    },
+    saveHero(){
+      this.message = JSON.stringify(this.hero, null, '💜');
+    }
+  },
 };
 </script>
