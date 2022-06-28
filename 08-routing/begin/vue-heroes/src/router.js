6b00dae78699ff8bdc81ccda886a6fa108f81420
Niki Theof <93220601+NikiTheofilaktidou@users.noreply.github.com>
2022-06-28 17:25:51 +0300

    routing, id parsed

--- 08-routing/begin/vue-heroes/src/router.js
+++ 08-routing/begin/vue-heroes/src/router.js
@@ -1,9 +1,10 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Heroes from './views/heroes.vue';
+import HeroDetail from './views/hero-detail.vue';
 
 Vue.use(Router);
-
+const parseProps = r => ({ id: parseInt(r.params.id) });
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
@@ -16,6 +17,13 @@ export default new Router({
       path: '/heroes',
       name: 'heroes',
       component: Heroes,
+    },
+    {
+      path: '/heroes/:id',
+      name: 'hero-detail',
+      component: HeroDetail,
+      //props: true,
+      props: parseProps,
     },
     {
       path: '/about',
